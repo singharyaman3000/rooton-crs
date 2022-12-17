bd6b44d9349493427d7a88abda1502fd7cbb3e7c
unknown <singharyaman3000@gmail.com>
2022-12-17 10:46:01 +0530

    change wagi file

--- backend/api.py
+++ backend/api.py
@@ -18,13 +18,9 @@ CORS = (app)
 
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, '../frontend/build/index.html')
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 #Create user
 @app.route('/userRegister', methods=['POST'])
